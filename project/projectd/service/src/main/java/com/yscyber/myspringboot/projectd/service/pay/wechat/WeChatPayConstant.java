b89daa9e8fefc52dfbb826418d2cd3efe38faa5e
yyscyber <40689257+yysCyber@users.noreply.github.com>
2019-12-07 17:43:41 +0800

    Update WeChatPayConstant.java

--- project/projectd/service/src/main/java/com/yscyber/myspringboot/projectd/service/pay/wechat/WeChatPayConstant.java
+++ project/projectd/service/src/main/java/com/yscyber/myspringboot/projectd/service/pay/wechat/WeChatPayConstant.java
@@ -12,17 +12,17 @@ public interface WeChatPayConstant {
     /**
      * 示例用，应用 ID
      */
-    String EXAMPLE_APP_ID = "wxd898fcb01713c658";
+    String EXAMPLE_APP_ID = "";
 
     /**
      * 示例用，商户 ID
      */
-    String EXAMPLE_MERCHANT_ID = "1483469312";
+    String EXAMPLE_MERCHANT_ID = "";
 
     /**
      * 示例用，商户密钥
      */
-    String EXAMPLE_MERCHANT_KEY = "098F6BCD4621D373CADE4E832627B4F6";
+    String EXAMPLE_MERCHANT_KEY = "";
 
     /**
      * 示例用，接收来自微信支付平台的支付通知（含支付最终结果等等）的 URL
@@ -30,17 +30,17 @@ public interface WeChatPayConstant {
      * 微信最终的支付结果，需要以来自微信支付平台的支付通知为准！！！
      * 要求：外网必须能够访问到
      */
-    String EXAMPLE_NOTIFY_URL = "http://yscyber.natapp1.cc/union-pay/wechat/native-second/async-notify";
+    String EXAMPLE_NOTIFY_URL = "";
 
     /**
      * 示例用，订单名称前缀
      */
-    String EXAMPLE_ORDER_NAME_PREFIX = "5328260-";
+    String EXAMPLE_ORDER_NAME_PREFIX = "xxxx-";
 
     /**
      * 示例用，收款方名称
      */
-    String EXAMPLE_ORDER_PAYEE = "杭州以北科技科技有限公司";
+    String EXAMPLE_ORDER_PAYEE = "";
 
     /**
      * 告诉微信，不要再发送异步通知了
